3bd2dbfebadebb3ddb5dcc9e9b36942c1700bf34
Chandan Sarkar <csarkar373@gmail.com>
2023-02-12 22:07:10 -0500

    working without priority queue

--- dijkstra.java
+++ dijkstra.java
@@ -1,11 +1,11 @@
 import java.util.*;
 
 public class Graph {
-    private int [] [] matrix;
+    private int[][] matrix;
     private int nodeCount;
-    boolean [] visited;
+    boolean[] visited;
 
-    public Graph(int [] [] matrix) {
+    public Graph(int[][] matrix) {
         this.matrix = matrix;
         this.nodeCount = matrix.length;
         this.visited = new boolean[this.nodeCount];
@@ -27,7 +27,9 @@ public class Graph {
         }
         return true;
     }
+
     public List<Integer> getNeighbors(int node) {
+        System.out.println("getting neighbors for node: " + node);
         String answer = "";
         List<Integer> answerList = new ArrayList<>();
         for (int i = 0; i < nodeCount; i++) {
@@ -55,9 +57,9 @@ public class Graph {
 
     public int getLowestUnvisitedNeighbor(int node) {
         List<Integer> unvisited = getUnvisitedNeighbors(node);
-        if (unvisited.size() == 0)
+        if (unvisited.size() == 0) {
             return -1; // all neighbors have been visited
-        else {
+        } else {
             int lowestIndex = 0;
             int lowestWeight = matrix[node][unvisited.get(0)];
             for (int i = 1; i < unvisited.size(); i++) {
@@ -73,10 +75,10 @@ public class Graph {
 
     public boolean hasEdge(int from, int to) {
         // -1 and MAX_VALUE are the two representations of infinity that we use
-        return matrix[from][to] != -1 && matrix[from][to] != Integer.MAX_VALUE;
+        return from != to && matrix[from][to] != -1 && matrix[from][to] != Integer.MAX_VALUE;
     }
 
-    public void printArray(int [] array) {
+    public void printArray(int[] array) {
         for (int i = 0; i < array.length; i++) {
             System.out.print(array[i] + " ");
         }
@@ -94,21 +96,30 @@ public class Graph {
         // set distance to start vertex to zero
         distances[startingNode] = 0;
         wasVisited(startingNode);
+        System.out.println("distances ");
+        printArray(distances);
 
         // while there remain some unvisited nodes
+        int current = startingNode;
         while (!allVisited()) {
-            int current = getLowestUnvisitedNeighbor(startingNode);
             for (int neighbor : getUnvisitedNeighbors(current)) {
-                int newDistance = distances[current] + matrix[current][neighbor];
-                if(newDistance < distances[neighbor]) {
-                    distances[current] = newDistance;
-                    nearest[neighbor] = current;
+                if (hasEdge(current, neighbor)) {
+                    int newDistance = distances[current] + matrix[current][neighbor];
+                    System.out.println("current = " + current +
+                            " neighbor = " + neighbor);
+                    System.out.println("new distance = " + newDistance +
+                            " old distance = " + distances[neighbor]);
+                    if (newDistance < distances[neighbor]) {
+                        distances[neighbor] = newDistance;
+                        nearest[neighbor] = current;
+                    }
                 }
             }
             wasVisited(current);
             printVisited();
             System.out.println("distances:");
             printArray(distances);
+            current = getLowestUnvisitedNeighbor(current);
         }
         // print the answers
         System.out.println("distances:");
@@ -142,41 +153,37 @@ public class Graph {
         String line = scan.nextLine();
         int graphCount = Integer.parseInt(line);
         System.out.println("graph count = " + graphCount);
-        line = scan.nextLine();
-        System.out.println("line = " + line);
-        String [] tokens = line.split(" ");
-        int nodes = Integer.parseInt(tokens[0]);
-        int edges = Integer.parseInt(tokens[1]);
-        System.out.println("nodes = " + nodes);
-        System.out.println("edges = " + edges);
-        int[][] matrix = new int[nodes][nodes];
-        for (int i = 0; i <nodes; i++) {
-            for (int j = 0; j < nodes; j++) {
-                matrix[i][j] = -1;
-            }
-        }
-        for (int j = 0; j < edges; j++) {
+        Graph[] graphs = new Graph[graphCount];
+        for (int trials = 0; trials < graphCount; trials++) {
             line = scan.nextLine();
-            tokens = line.split(" ");
-            int from = Integer.parseInt(tokens[0]);
-            int to = Integer.parseInt(tokens[1]);
-            if (tokens.length == 3) { // weighted graph
-                matrix[from][to] = Integer.parseInt(tokens[2]);
-                matrix[to][from] = Integer.parseInt(tokens[2]);
-            } else { // unweighted graph; use default weight of 1
-                matrix[from][to] = 1;
-                matrix[to][from] = 1;
+            System.out.println("line = " + line);
+            String[] tokens = line.split(" ");
+            int nodes = Integer.parseInt(tokens[0]);
+            int edges = Integer.parseInt(tokens[1]);
+            System.out.println("nodes = " + nodes);
+            System.out.println("edges = " + edges);
+            int[][] matrix = new int[nodes][nodes];
+            for (int i = 0; i < nodes; i++) {
+                for (int j = 0; j < nodes; j++) {
+                    matrix[i][j] = -1;
+                }
             }
+            for (int j = 0; j < edges; j++) {
+                line = scan.nextLine();
+                tokens = line.split(" ");
+                int from = Integer.parseInt(tokens[0]);
+                int to = Integer.parseInt(tokens[1]);
+                if (tokens.length == 3) { // weighted graph
+                    matrix[from][to] = Integer.parseInt(tokens[2]);
+                    matrix[to][from] = Integer.parseInt(tokens[2]);
+                } else { // unweighted graph; use default weight of 1
+                    matrix[from][to] = 1;
+                    matrix[to][from] = 1;
+                }
+            }
+            printMatrix(matrix);
+            graphs[trials] = new Graph(matrix);
+            graphs[trials].dijkstra(0);
         }
-        printMatrix(matrix);
-        Graph graph = new Graph(matrix);
-        graph.dijkstra(0);
-
-        /*
-        for (int j = 0; j < graph.matrix.length; j++) {
-            graph.dfs(j);
-        }
-
-         */
     }
 }
